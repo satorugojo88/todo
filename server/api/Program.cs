fa7c8ca6cf5cf9e2d9c9d89b06855b7c21482166
Nathaniel Coleman <satorugojo8868@outlook.com>
2026-05-31 09:15:16 -0400

    configuration

--- server/api/Program.cs
+++ server/api/Program.cs
@@ -1,6 +1,43 @@
+using System.Text.Json;
+using api;
+using efscaffold;
+using efscaffold.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
+var appOptions = builder.Services.AddAppOptions(builder.Configuration);
+
+Console.WriteLine(JsonSerializer.Serialize(appOptions));
+
+builder.Services.AddDbContext<TodoDbContext>(options =>
+{
+  options.UseNpgsql(appOptions.DbConnectionString);
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", (
+  [FromServices]IOptionsMonitor<AppOptions> optionsMonitor,
+  [FromServices]TodoDbContext db
+  ) =>
+{
+  var myTodo = new Todo
+  {
+    Id = Guid.NewGuid().ToString(),
+    Description = "Getting things done mindset",
+    Title = "Organize my daily routine",
+    Completed = false,
+    Priority = 5,
+    CreatedAt = DateTime.Now
+  };
+  db.Todos.Add(myTodo);
+  db.SaveChanges();
+  
+  var results = db.Todos.ToList();
+  
+  return results;
+});
 
 app.Run();
